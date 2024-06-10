46f765b6adbfde7754568642a4c46b4225da2f85
Erik Veenstra <zerikveenstraz@gmail.com>
2024-06-10 21:01:35 +0200

    Added missing fields to models, returned image now correctly displayed when received.

---
+++ Migrations/20240610180826_AddedFields.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeneratorClient.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ErrorMessage",
+                table: "GenerationResponses",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RequestId",
+                table: "GenerationResponses",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Success",
+                table: "GenerationResponses",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GenerationResponses_RequestId",
+                table: "GenerationResponses",
+                column: "RequestId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GenerationResponses_GenerationRequests_RequestId",
+                table: "GenerationResponses",
+                column: "RequestId",
+                principalTable: "GenerationRequests",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GenerationResponses_GenerationRequests_RequestId",
+                table: "GenerationResponses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GenerationResponses_RequestId",
+                table: "GenerationResponses");
+
+            migrationBuilder.DropColumn(
+                name: "ErrorMessage",
+                table: "GenerationResponses");
+
+            migrationBuilder.DropColumn(
+                name: "RequestId",
+                table: "GenerationResponses");
+
+            migrationBuilder.DropColumn(
+                name: "Success",
+                table: "GenerationResponses");
+        }
+    }
+}
